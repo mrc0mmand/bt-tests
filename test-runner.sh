1f08a822bc72a31c9085131534407837f8ab308d
Frantisek Sumsal <frantisek@sumsal.cz>
2016-10-17 10:27:13 +0200

    Properly setup workspace

--- test-runner.sh
+++ test-runner.sh
@@ -2,7 +2,8 @@
 
 yum -y makecache
 # TODO: epel-release is not in fedora
-yum -y install epel-release openssl nss gnutls net-tools coreutils gnutls-utils
+yum -y install epel-release openssl nss gnutls net-tools coreutils \
+               gnutls-utils expect
 yum -y install beakerlib
 
 EC=0
@@ -15,12 +16,14 @@ for test in ${TESTS[@]}; do
     TESTPATH="/workspace/$test"
     echo "Running test: $test"
     echo "--------------------------------------"
-    chmod +x $TESTPATH
-    TERM=dumb $TESTPATH
+    chmod +x "$TESTPATH"
+    pushd "$(dirname "$TESTPATH")"
+    "$TESTPATH"
     if [[ $? -ne 0 ]]; then
         EC=1
     fi
     echo -e "\n"
+    popd
 done
 
 exit $EC
