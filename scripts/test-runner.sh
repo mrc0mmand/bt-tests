710d333ca23af5ef2bdb5060567b0a7d4f0f83f5
Frantisek Sumsal <frantisek@sumsal.cz>
2017-01-18 18:59:38 +0100

    Install the latest version of beakerlib

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -56,6 +56,12 @@ yum -y install https://sumsal.cz/dev/beakerlib-1.11centos-99.fc23.noarch.rpm
 $PKG_MAN -y install net-tools coreutils gawk expect make findutils \
                     procps-ng
 
+if [[ $OS_TYPE == "fedora" ]]; then
+    $PKG_MAN --enablerepo updates-testing -y install beakerlib
+else
+    $PKG_MAN --enablerepo epel-testing -y install beakerlib
+fi
+
 # WORKAROUND: Replace all rlIsRHEL calls with rlIsCentos
 if [[ $OS_TYPE == "centos" ]]; then
     echo 'rlIsRHEL() { rlIsCentOS "$@"; }' >> /usr/share/beakerlib/testing.sh
